e2994251394095cd133b32276bbc758b57817e87
andrea-livornese <andrealivornese@gmail.com>
2024-03-01 16:41:04 +0100

    creazione dell'item dei comics e aggiunto jumbotron

---
+++ src/components/AppComicsItem.vue
@@ -0,0 +1,32 @@
+<script>
+
+export default{
+  name:"AppComicsItem",
+
+  data(){
+    return{
+       
+        
+    }
+  },
+}
+</script>
+
+<template>
+ 
+    <div class="card">
+        <div class="card-img">
+            
+        </div>
+        <div class="comics-title">
+
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss">
+@use '../style/mixins' as *;
+
+
+</style>
